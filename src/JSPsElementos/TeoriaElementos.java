bbdde3bf3e241ff3770847c97cd1bd13f28bf028
Stiven106 <brayan106-@hotmail.com>
2021-04-01 18:31:47 -0500

    Update TeoriaElementos.java

--- src/JSPsElementos/TeoriaElementos.java
+++ src/JSPsElementos/TeoriaElementos.java
@@ -4,33 +4,38 @@ public class TeoriaElementos {
     /*
                     ELEMENTOS DE UN JSP
 
-        Expressions:
+        Expressions:evaluadas e insertadas en la salida del servlet.
+
             *Sintaxis: <%= expresion %>
             *La expresion se evalua y se inserta en la salida del servlet
             *Es equivalente a out.println(expresion)
 
-        Scriptlets: Un scriplet puede tener codigo Java que no necesariamente va a visualizar el cliente. Demasiado codigo en un scriplet
-        se considera una mala practica.
-            *Sintaxis: <% codigoJava %>
+        Scriptlets: Un scriplet puede tener codigo Java que no necesariamente va a visualizar el cliente.
+        Permiten incrustar código escrito en otro lenguaje de programación. Demasiado codigo en un scriplet se considera una mala practica.
+
+            *Sintaxis: <% codigoJava %> : evalúa la expresión y muestra el resultado en la página.
             *El codigo Java se inserta en el metodo service() del Servlet generado
             *Puede ser cualquier codigo Java valido
 
-        Declaraciones: El codigo que agreguemos, si es que agregamos una variable, se vuelve una variable de instancia. Debido a que se esta agregando
+        Declaraciones: el código es insertado en la clase del servlet, fuera de los métodos.
+        El codigo que agreguemos, si es que agregamos una variable, se vuelve una variable de instancia. Debido a que se esta agregando
         como una variable del Servlet generado y no unicamente como una variable local de cierto metodo.
         Este concepto no es muy utilizado, pero se utiliza siempre y cuando necesitemos agregar variables o metodos que pertenezcan a nuestro
         Servlet generado a partir del JSP.
+
             *Sintaxis: <%! codigoJava %>
             *Se utiliza para agregar codigo a la clase del Servlet generado.
             *Se pueden declarar variables o metodos que pertenecen a la clase
 
         Sintaxis XML:
+
             *Cada elemento JSP tiene su equivalente en sintaxis XML
             *Esta sintaxis se utiliza para tener una mayor compatibilidad, por ejemplo, con herramientas visuales.
 
 
 
         Expresion: Puede ser una cadena, expresion matematica, o el resultado de una llamada a una funcion. Pero este tiene que regresar un
-        valor.
+        valor, no puede ser null.
 
                     VARIABLES IMPLICITAS EN LOS JSP'S
 
