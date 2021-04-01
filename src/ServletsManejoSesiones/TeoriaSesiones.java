77216e9ba59c881a8bb48a16870cffff6053d1ea
Stiven106 <brayan106-@hotmail.com>
2021-04-01 10:30:37 -0500

    Sesiones con Servlets
    Concepto-Teoría

--- src/ServletsManejoSesiones/TeoriaSesiones.java
+++ src/ServletsManejoSesiones/TeoriaSesiones.java
@@ -1,59 +1,40 @@
 package ServletsManejoSesiones;
 
 public class TeoriaSesiones {
-
     /*
+
+                SESION HTTP
+
+    Nos permite administrar las sesiones de los clientes. Se obtiene a partir del objeto HttpServletRequest.
 
                 MANEJO DE SESIONES CON SERVLETS
 
-    *Una sesion en una aplicacion Web permite administrar varias peticiones de un mismo usuario.
+    request.getSession(): Se utiliza para obtener la sesion que se creo a partir de la peticion del cliente.
 
-    *La necesidad surge debido a que el protocolo HTTP es un protocolo sin estado, esto significa que entre peticion  y peticion no guarda
-    ninguna informacion del usuario que realizo dicha peticion.
+    sesion.getAttribute(): Permite obtener un atributo previamente agregado a la sesion del cliente.
 
-    *Las sesiones en los Servlets se pueden manejar por medio de dos mecanismos:
-        *Cookies
-        *URL Rewritting
+    sesion.setAttribute(): permite agregar un atributo a la sesion actual del cliente.
 
-    *El API de los Servlets abstrae estos conceptos en una clase llamada HTTPSession.
+    sesion.removeAttribute(): Permite eliminar un atributo agregado a la sesion
 
-                CONCEPTO Y USO DE COOKIES
+    sesion.invalidate(): Invalida la sesion actual del cliente.
 
-        *Una cookie es un archivo que contiene informacion en forma de nombre y valor, el cual se almacena en el navegador Web.
+                MANEJO DE SESIONES CON SERVLETS
 
-        *El objetivo de una Cookie es almacenar informacion del usuario cada vez que accede a un mismo sitio, como puede ser el lenguaje
-        preferido, colores preferidos y en general las preferencias de usuario.
+            OTROS METODOS IMPORTANTES
 
-        *Las cookies no se deben utilizar para almacenar informacion sensible como passwords, tarjetas de credito, etc, debido a que la
-        informacion almacenada es texto plano sin ningun tipo de encriptacion.
+    sesion.isNew(): Permite saber si la sesion ha sido recien creada.
 
+    sesion.getCreationTime(): Permite conocer la fecha y hora de cuando se creo la sesion
 
-                API DE LAS COOKIES EN LOS SERVLETS
+    sesion.getLastAccesedTime(): Permite conocer la ultima vez en que la sesion fue accedida por el cliente.
 
-        *Un Cookie se puede leer en una peticion HTTP (se itera el arreglo de Cookies) y se puede escribir en una respuesta HTTP.
+    sesion.getMaxInactiveInterval(): Permite conocer el tiempo de inactividad (en segundos) necesario para que la sesion se destruya si
+    no recibe una peticion.
 
-        *Creacion de un objeto Cookie:
-            Cookie c = new Cookie("usuario","Juan");
-            c.getName();
-            c.getValue();
-
-        *Metodos para leer todas las cookies en una peticion HTTP:
-            Cookie[] cookies = request.getCookies();
-
-        *Metodo para agregar una Cookie en la respuesta:
-            response.addCookie(c);
-
-                API DEL OBJETO COOKIE
-
-        *A continuacion se mencionan los metodos mas comunes en un objeto Cookie:
-
-            *getDomain/setDomain: Se utiliza para especificar el dominio de donde proviene o se va a almacenar la Cookie
-            *getMaxAge/setMaxAge: Especifica el tiempo a expirar (segundos)
-            *getName: Obtiene el nombre de la cookie, para colocar el nombre se debe usar el constructor de la clase
-            *getValue/setValue: Especifica el valor asociado con el nombre de la Cookie
-
+    sesion.setMaxInactiveInterval(): Permite modificar el valor mencionado en la funcion anterior. Este valor tambien se puede modificar en el
+    archivo web.xml.
 
      */
-
 
 }
